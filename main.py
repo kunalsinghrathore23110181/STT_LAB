85b3924147965079ec17ea504b5a8cd930cdb3da
kunalsinghrathore23110181 <156738501+kunalsinghrathore23110181@users.noreply.github.com>
2025-08-18 23:52:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+class Calc:
+    def __init__(self):
+        self.a, self.b = 12, 7
+
+    def add(self):
+        return self.a + self.b
+
+    def sub(self):
+        return self.a - self.b
+
+    def mul(self):
+        return self.a * self.b
+
+    def div(self):
+        return self.a / self.b
+
+    def run(self):
+        print(self.add())
+        print(self.sub())
+        print(self.mul())
+        print(self.div())
+
+
+if __name__ == "__main__":
+    obj = Calc()
+    obj.run()
 
